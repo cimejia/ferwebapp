5a3ba0e1f1e3d655c94f961b54ca0673e7e1a35a
Christian <cimejia@uce.edu.ec>
2022-01-20 17:36:22 -0500

    8 commit

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import torch
 import numpy as np
-#from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -19,13 +17,7 @@ def hello_world():
 def predict():
 
     # load image
- #   img = Image.open(request.files['file'].stream).convert('RGB').resize((224, 224))
-
-    img=mpimg.imread(request.files['file'].stream)
-    img=img.astype('float32')
-    #img=img/255
-    
-
+    img = Image.open(request.files['file'].stream).convert('RGB').resize((224, 224))
     img = np.array(img)
     img = torch.FloatTensor(img.transpose((2, 0, 1)) / 255)
 
