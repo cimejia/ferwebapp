abb638a3f853dd502273202596fe7944176cc4df
Christian <cimejia@uce.edu.ec>
2022-01-19 19:44:51 -0500

    3 commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -8,5 +9,14 @@ CORS(app)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/prediction')
+def prediction():
+
+    # load image
+    #img = Image.open(request.files['file'].stream).convert('RGB').resize((224, 224))
+    #img = np.array(img)
+    return 'Hello, prediction!'
+
+        
 if __name__ == '__main__':
     app.run()
